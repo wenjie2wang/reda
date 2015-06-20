a24f8064515d28986db714f8f89e304a7e5e2c9a
wenjie2wang <wenjie.2.wang@uconn.edu>
2015-06-19 22:50:20 -0400

    updated R code arrangement and DESCRIPTION file

---
+++ R/heart.R
@@ -0,0 +1,12 @@
+#' heart: A package for parametric recurrent event analysis .
+#'
+#' The heart package provides three categories of important functions:
+#' fitheart, plot and so on.
+#' 
+#' @section fitheart functions:
+#' The fitheart functions ...
+#'
+#' @docType package
+#' @name fitheart
+NULL
+#> NULL
