f043f48d31af2b53c87ac4956349c27d265e481c
wenjie2wang <wenjie.2.wang@uconn.edu>
2015-06-25 01:01:43 -0400

    added embeded README

--- R/plot_heart.R
+++ R/plot_heart.R
@@ -1,3 +1,26 @@
+##############################################################################
+##
+##   R package heart by Haoda Fu, Jun Yan, and Wenjie Wang
+##   Copyright (C) 2015
+##
+##   This file is part of the R package heart.
+##
+##   The R package heart is free software: you can redistribute it and/or
+##   modify it under the terms of the GNU General Public License as published
+##   by the Free Software Foundation, either version 3 of the License, or
+##   (at your option) any later version.
+##
+##   The R package heart is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+##
+##   You should have received a copy of the GNU General Public License
+##   along with the R package heart. If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+
 #' plot baseline.
 #' 
 #' \code{plot} produces the plot of baseline function.
