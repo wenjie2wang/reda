36fe25704e4ff1834f30deeec2790ccb88f060b0
wenjie2wang <wenjie.2.wang@uconn.edu>
2015-07-12 15:44:49 -0400

    added simulated dataset for demonstration

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Simulated Dataset for Demonstration
+#'
+#' A data frame with covariates named 
+#' 'ID', 'time', 'event', 'group' and 'X1'.
+#' 
+#' The dataset is simulated by the thinning method developed 
+#' by \emph{Lewis and Shedler (1979)}. 
+#' See \emph{Fu et al. (2014)} for more details.
+#' @docType data
+#' @name simuDat
+#' @format data frame
+#' @references 
+#' Lewis, P. and Shedler, G. (1979), 
+#' “Simulation of nonhomogeneous Poisson processes by thinning,” 
+#' \emph{Naval Research Logistics Quarterly}, 26, 403–413.
+#' 
+#' Fu, Haoda, Junxiang Luo, and Yongming Qu. (2014),
+#' "Hypoglycemic Events Analysis via Recurrent Time-to-Event (HEART) Models," 
+#' \emph{Journal of biopharmaceutical statistics}, 2014 Dec 1, Epub 2014 Dec 1.
+#' @examples 
+#' data(simuDat)
+#' str(simuDat)
+NULL
