6aa58a264fb589971ab86f197d807f9f23685051
Wenjie Wang <wenjie.2.wang@uconn.edu>
2016-11-24 12:52:40 -0500

    add valve-seat data

--- R/data.R
+++ R/data.R
@@ -21,14 +21,15 @@
 ##' Simulated Sample Dataset for Demonstration
 ##'
 ##' A simulated data frame with covariates named
-##' \code{ID}, \code{time}, \code{event}, \code{group} and \code{x1},
-##' where
+##' \code{ID}, \code{time}, \code{event}, \code{group}, \code{x1},
+##' and \code{gender}, where
 ##' \itemize{
 ##'     \item \code{ID}: Subjects identification;
 ##'     \item \code{time}: Event or censoring time;
 ##'     \item \code{event}: Event indicator, 1 = event, 0 = censored;
 ##'     \item \code{group}: Treatment group indicator;
 ##'     \item \code{x1}: Continuous variable.
+##'     \item \code{gender}: Gender of subjects.
 ##' }
 ##'
 ##' @details
@@ -39,7 +40,7 @@
 ##'
 ##' @docType data
 ##' @name simuDat
-##' @format A data frame with 500 rows and 5 variables.
+##' @format A data frame with 500 rows and 6 variables.
 ##' @references
 ##' Lewis, P. A., & Shedler, G. S. (1979).
 ##' Simulation of nonhomogeneous Poisson processes by thinning.
@@ -48,4 +49,27 @@
 ##' Fu, H., Luo, J., & Qu, Y. (2016).
 ##' Hypoglycemic events analysis via recurrent time-to-event (HEART) models.
 ##' \emph{Journal Of Biopharmaceutical Statistics}, 26(2), 280--298.
+NULL
+
+
+##' Valve Seats Dataset
+##'
+##' Valve seats wear out in certain diesel engines, each with 16 valve seats.
+##' The dataset served as an example of recurrence data in Nelson (1995),
+##' which consists of valve-seat replacements on 41 engines in a fleet.
+##' The covariates are named
+##' \code{ID}, \code{Days}, and \code{No.}, where
+##' \itemize{
+##'     \item \code{ID}: The engine number;
+##'     \item \code{Days}: Engine age in days;
+##'     \item \code{No.}: Event indicator, '1' for a valve-seat replacement
+##'         and, '0' for the censoring age of an engine.
+##' }
+##' @docType data
+##' @name valveSeats
+##' @format A data frame with 89 rows and 3 variables.
+##' @references
+##' Nelson, W. (1995), Confidence Limits for Recurrence
+##' Data-Applied to Cost or Number of Product Repairs, \emph{Technometrics},
+##' 37, 147--157.
 NULL
