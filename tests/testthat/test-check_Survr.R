6dbc1c78d632ec1098308d5982dd4a2aac528f7c
Wenjie Wang <wenjie.2.wang@uconn.edu>
2017-10-05 22:39:34 -0400

    minor updates on test script

--- tests/testthat/test-check_Survr.R
+++ tests/testthat/test-check_Survr.R
@@ -8,23 +8,23 @@ test_that("call reda::Survr", {
     tmpDat <- simuDat
     tmpDat[c(5, 10), "event"] <- 1
     expect_error(with(tmpDat, Survr(CID, time, event)),
-                 "Please check subject: A1, A4.", fixed = TRUE)
+                 "A1, A4.", fixed = TRUE)
 
     tmpDat <- simuDat
     tmpDat[c(27, 30), "time"] <- 1
     expect_error(with(tmpDat, Survr(CID, time, event)),
-                 "Please check subject: A5, A6.", fixed = TRUE)
+                 "A5, A6.", fixed = TRUE)
 
     ## issue 2: more than one censoring time
     tmpDat <- simuDat
     tmpDat[c(25, 28), "event"] <- 0
     expect_error(with(tmpDat, Survr(CID, time, event)),
-                 "Please check subject: A5, A6.", fixed = TRUE)
+                 "A5, A6.", fixed = TRUE)
 
     ## error caused after moving observation containing missing values
     tmpDat <- simuDat
     tmpDat[c(8, 94), "x1"] <- NA
     expect_error(rateReg(Survr(CID, time, event) ~ x1, tmpDat),
-                 "Please check subject: A16, A3.", fixed = TRUE)
+                 "A16, A3.", fixed = TRUE)
 
 })
