309047e86dc1238e1d4dcc5523d16c2e5bd7b4ef
Wenjie Wang <wenjie.2.wang@uconn.edu>
2017-12-12 20:58:32 -0500

    minor updates

--- src/relativeRisk.cpp
+++ src/relativeRisk.cpp
@@ -3,13 +3,12 @@
 
 #include<Rcpp.h>
 
-
 // [[Rcpp::export]]
 Rcpp::NumericVector rrisk_exponential(arma::mat z, arma::mat zCoef)
 {
   unsigned int nrow_z {z.n_rows};
   Rcpp::NumericVector res(nrow_z);
-  for (size_t i {0}; i < z.n_rows; ++i) {
+  for (size_t i {0}; i < nrow_z; ++i) {
     res[i] = std::exp(arma::sum(z.row(i) % zCoef.row(i)));
   }
   return res;
@@ -20,7 +19,7 @@ Rcpp::NumericVector rrisk_linear(arma::mat z, arma::mat zCoef)
 {
   unsigned int nrow_z {z.n_rows};
   Rcpp::NumericVector res(nrow_z);
-  for (size_t i {0}; i < z.n_rows; ++i) {
+  for (size_t i {0}; i < nrow_z; ++i) {
     res[i] = 1 + arma::sum(z.row(i) % zCoef.row(i));
   }
   return res;
@@ -31,7 +30,7 @@ Rcpp::NumericVector rrisk_excess(arma::mat z, arma::mat zCoef)
 {
   unsigned int nrow_z {z.n_rows};
   Rcpp::NumericVector res(nrow_z);
-  for (size_t i {0}; i < z.n_rows; ++i) {
+  for (size_t i {0}; i < nrow_z; ++i) {
     res[i] = std::exp(arma::sum(arma::log(1 + z.row(i) % zCoef.row(i))));
   }
   return res;
