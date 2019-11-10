8dfe9f5e03d282212b96a4cb4ecbd37757083153
Wenjie Wang <wjwang.stat@gmail.com>
2019-11-10 11:04:20 -0500

    correct docs path

--- misc/deploy_docs.sh
+++ misc/deploy_docs.sh
@@ -4,10 +4,11 @@ set -e
 
 ## run on wenjie's droplets
 build_dir=$(pwd)
-
 cd $HOME/wenjie/wenjie-stat.me/
+git checkout -f
+git checkout master
 git pull origin master
 cp -r $build_dir/docs/* $HOME/wenjie/wenjie-stat.me/static/reda/
-git add -u docs/
+git add -u static/reda/
 git commit -m "deploy $CI_COMMIT_SHORT_SHA by gitlab-runner"
 git push origin master
