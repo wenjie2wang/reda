46c04ffcfd480997bda228c94e7af39c66865a15
Wenjie Wang <wjwang.stat@gmail.com>
2019-11-10 16:28:47 -0500

    tweak ci job for docs deploy

--- misc/deploy_docs.sh
+++ misc/deploy_docs.sh
@@ -9,6 +9,14 @@ git checkout -f
 git checkout master
 git pull origin master
 cp -r $build_dir/docs/* $HOME/wenjie/wenjie-stat.me/static/reda/
-git add -u static/reda/
-git commit -m "deploy reda $CI_COMMIT_SHORT_SHA by gitlab-runner"
-git push origin master
+tmp_log=.git_status.log
+git status > $tmp_log
+if egrep -q "modified:[ ]+docs/" $tmp_log
+then
+    git add -u static/reda/
+    git commit -m "deploy reda $CI_COMMIT_SHORT_SHA by gitlab-runner"
+    git push origin master
+else
+    printf "The docs was not updated.\n"
+fi
+rm $tmp_log
