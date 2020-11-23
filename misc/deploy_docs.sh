e009790f0ba848b64ffba62004244031f38260bb
Wenjie Wang <wang@wwenjie.org>
2020-11-23 14:25:53 -0500

    minor updates on gitlab ci

--- misc/deploy_docs.sh
+++ misc/deploy_docs.sh
@@ -5,7 +5,7 @@ set -e
 ## run on wenjie's droplets
 pkg=$(grep "Package" DESCRIPTION | awk '{print $NF}')
 build_dir=$(pwd)
-docs_repo=$HOME/wenjie/wenjie-stat.me
+docs_repo=$HOME/wenjie/wwenjie.org
 target_dir=$docs_repo/static/$pkg
 tmp_log=.git_status.log
 
@@ -20,9 +20,10 @@ git pull origin master
 mkdir -p $target_dir
 cp -r $build_dir/docs/* $target_dir
 git status > $tmp_log
+cat $tmp_log
 if egrep -q "modified:[ ]+static/$pkg/" $tmp_log
 then
-    git add -u static/$pkg/
+    git add static/$pkg/
     git commit -m "deploy $pkg $CI_COMMIT_SHORT_SHA by gitlab-runner"
     git push origin master
 else
